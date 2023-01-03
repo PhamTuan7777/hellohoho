48fd5cd72db35c63960af1c0de7a8b7f401ffeb4
Pham Tuan <tuan.phamquang@meiko.vn>
2023-01-03 10:04:57 +0700

    add new lib

--- haha.cpp
+++ haha.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 int main()
 {
+    int a = 0;
+    int b[10];
+    cout << "nhap so luong phan tu";
+    cin >> a;
     system("pause");
     return 0;
 }
