f8c0ce213699c9e6760cb101c27b5f8ac45f14ad
Pham Tuan <tuan.phamquang@meiko.vn>
2023-01-03 15:43:56 +0700

    add more content

--- haha.cpp
+++ haha.cpp
@@ -8,7 +8,13 @@ int main()
     int b[10];
     cout << "nhap so luong phan tu: ";
     cin >> a;
+    
     cout << "so luong phan tu cua mang la: " << a;
+    for(int i = 0; i < a; i++)
+    {
+        cout << " ";
+        cin >> b[i];
+    }
     for(int i = 0; i < a; i++)
     {
         cout << "phan tu so " << i << "la: ";
